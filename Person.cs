9963487f6dd8486442581e1fbe1c453479cf7b4a
alhabsi16 <mxmx8283@gmail.com>
2023-07-12 11:46:51 +0400

    Sort and Reverse

--- Person.cs
+++ Person.cs
@@ -6,18 +6,38 @@ using System.Threading.Tasks;
 
 namespace C_Sharp_List
 {
-    internal class Person
+    internal class Person : IComparable<Person>
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Gander { get; set; }
+        public int Age { get; set; }
 
 
-        public Person(int Id, string Name, string Gander) 
+        public Person(int Id, string Name, string Gander, int Age) 
         {
             this.Id = Id;
             this.Name = Name;
             this.Gander = Gander;
+            this.Age = Age;
+        }
+
+        public int CompareTo(Person? other)
+        {
+            if (other == null)
+            { 
+            return 1;
+            }
+            if (other.Age == this.Age) 
+            {
+                return 0;
+            }
+            if (other.Age > this.Age)
+            {
+                return -1;
+            }
+            return 1;
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,42 +4,67 @@
     {
         static void Main(string[] args)
         {
-            //List<int> list = new List<int>();
-            //list.Add(1);
-            //Console.WriteLine($"Size of the list is {list.Count}");
-            //list.Add(2);
-            //Console.WriteLine($"Size of the list is {list.Count}");
-            //list.Remove(1);
-            //Console.WriteLine($"Size of the list is {list.Count}");
-            //list.Insert(0,5);
-            //Console.WriteLine($"Size of the list is {list.Count}");
-            //Console.WriteLine(list[0]);
-            //foreach(int  i in list) 
-            //Console.WriteLine(i);
-
-
-            //List<Person> personList = new List<Person>();
-            //personList.Add(new Person(1122,"Mohammed","Male"));
-            //personList.Add(new Person(1133, "Ahmed", "Male"));
-            //personList.Add(new Person(1144, "Anood", "Female"));
-            //foreach (Person person in personList)
-            //{
-            //    Console.WriteLine(person.Id +" " + person.Name + " " + person.Gander);
-            //}
-
-            List<int> numberList = new List<int>();
-            numberList.Add(4);
-            numberList.Add(2);
-            numberList.Add(10);
-            numberList.Add(8);
-            numberList.Add(12);
-            numberList.Add(6);
-
-            numberList.Sort();
-            foreach (int i in numberList)
-            {
+            List<int> list = new List<int>();
+            list.Add(1);
+            Console.WriteLine($"Size of the list is {list.Count}");
+            list.Add(2);
+            Console.WriteLine($"Size of the list is {list.Count}");
+            list.Remove(1);
+            Console.WriteLine($"Size of the list is {list.Count}");
+            list.Insert(0, 5);
+            Console.WriteLine($"Size of the list is {list.Count}");
+            Console.WriteLine(list[0]);
+            foreach (int i in list)
+            { 
                 Console.WriteLine(i);
             }
+            Console.WriteLine("========================");
+
+            List<Person> personList = new List<Person>();
+            personList.Add(new Person(1122, "Mohammed", "Male", 32));
+            personList.Add(new Person(1133, "Ahmed", "Male", 20));
+            personList.Add(new Person(1144, "Anood", "Female", 25));
+            foreach (Person person in personList)
+            {
+                Console.WriteLine($"Id: { person.Id}, Name: { person.Name}, Gander: { person.Gander}, Age:  { person.Age}");
+                Console.WriteLine("--------------------------");
+            }
+
+            List<int> numberList = new List<int>();
+            numberList.Add(2);
+            numberList.Add(1);
+            numberList.Add(5);
+            numberList.Add(4);
+            numberList.Add(6);
+            numberList.Add(3);
+
+            Console.WriteLine("========================");
+            numberList.Sort(); // To Arrangement the number
+            foreach (int i in numberList)
+            {
+                Console.WriteLine( "Sort: " + i);
+            }
+
+            Console.WriteLine("========================");
+            numberList.Sort(); // To Arrangement the number
+            numberList.Reverse(); // To write reverse number (A large number to a small number)
+            foreach (int i in numberList)
+            {
+                Console.WriteLine("Reverse: " + i);
+            }
+
+            Console.WriteLine("========================");
+            personList.Sort();
+            Console.WriteLine("The name of the youngest name to oldest name:");
+            foreach (Person person in personList)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            //Console.WriteLine();
+            //Console.WriteLine("========================");
+
+
         }
     }
 }
