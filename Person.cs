d37048f7dbc405dc9a9e98e91bfd5de224a2b792
alhabsi16 <mxmx8283@gmail.com>
2023-07-12 10:38:05 +0400

    List

---
+++ Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_List
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gander { get; set; }
+
+
+        public Person(int Id, string Name, string Gander) 
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Gander = Gander;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace C_Sharp_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            Console.WriteLine($"Size of the list is {list.Count}");
+            list.Add(2);
+            Console.WriteLine($"Size of the list is {list.Count}");
+            list.Remove(1);
+            Console.WriteLine($"Size of the list is {list.Count}");
+            list.Insert(0,5);
+            Console.WriteLine($"Size of the list is {list.Count}");
+            Console.WriteLine(list[0]);
+            foreach(int  i in list) 
+            Console.WriteLine(i);
+
+
+            List<Person> personList = new List<Person>();
+            personList.Add(new Person(1122,"Mohammed","Male"));
+            personList.Add(new Person(1133, "Ahmed", "Male"));
+            personList.Add(new Person(1144, "Anood", "Female"));
+            foreach (Person person in personList)
+            {
+                Console.WriteLine(person.Id +" " + person.Name + " " + person.Gander);
+            }
+        }
+    }
+}
